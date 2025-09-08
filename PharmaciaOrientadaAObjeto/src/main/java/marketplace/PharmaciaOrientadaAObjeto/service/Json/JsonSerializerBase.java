ba71733a7bf8c32726aaec13ae00536309fa77a9
RaphaelLuizPH <130625114+RaphaelLuizPH@users.noreply.github.com>
2025-09-07 21:13:19 -0300

    Ajustes de JSON

--- PharmaciaOrientadaAObjeto/src/main/java/marketplace/PharmaciaOrientadaAObjeto/service/Json/JsonSerializerBase.java
+++ PharmaciaOrientadaAObjeto/src/main/java/marketplace/PharmaciaOrientadaAObjeto/service/Json/JsonSerializerBase.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
 
+@Converter(autoApply = true)
 public class JsonSerializerBase<T> implements AttributeConverter<T, String> {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
