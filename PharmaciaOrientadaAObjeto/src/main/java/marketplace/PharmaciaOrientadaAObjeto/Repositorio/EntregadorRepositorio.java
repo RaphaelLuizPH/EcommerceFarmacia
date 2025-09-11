b7d0f450f281ca16f532abd86c1d31b8596fef7a
RaphaelLuizPH <130625114+RaphaelLuizPH@users.noreply.github.com>
2025-09-10 22:35:21 -0300

    Added custom query

--- PharmaciaOrientadaAObjeto/src/main/java/marketplace/PharmaciaOrientadaAObjeto/Repositorio/EntregadorRepositorio.java
+++ PharmaciaOrientadaAObjeto/src/main/java/marketplace/PharmaciaOrientadaAObjeto/Repositorio/EntregadorRepositorio.java
@@ -3,10 +3,13 @@ package marketplace.PharmaciaOrientadaAObjeto.Repositorio;
 
 import marketplace.PharmaciaOrientadaAObjeto.model.Usuario.Entregador;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface EntregadorRepositorio extends JpaRepository<Entregador, String> {
-    
-    Optional<Entregador> findByCNH(String cnh);
+
+    @Query("SELECT e FROM Entregador AS E WHERE E.CNH = :cnh")
+    Optional<Entregador> findByCNH(@Param("cnh") String cnh);
 }
