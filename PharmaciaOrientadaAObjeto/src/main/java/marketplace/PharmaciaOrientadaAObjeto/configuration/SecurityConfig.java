6faafa4d58dc915a5e83844180a7c70b9e5dcded
Raphael Luiz <bioshockx6@outlook.com>
2025-11-10 16:15:59 -0300

    adicionei docker

---
+++ PharmaciaOrientadaAObjeto/src/main/java/marketplace/PharmaciaOrientadaAObjeto/configuration/SecurityConfig.java
@@ -0,0 +1,41 @@
+package marketplace.PharmaciaOrientadaAObjeto.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http .authorizeHttpRequests(requests ->
+                        requests.anyRequest().permitAll()
+                )
+                .cors(Customizer.withDefaults()) // enables CORS
+                .csrf(AbstractHttpConfigurer::disable);   // disables CSRF
+        return http.build();
+
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:80"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
