8520c0e2ccffe3e56eebf4d083783f2e3ac67c3b
Raphael Luiz <bioshockx6@outlook.com>
2025-11-17 12:59:08 -0300

    Reapply "Correçao do arquivo banco_dados padronizando com a atual estrutura do projeto"
    This reverts commit ef6b74dd3c667464a0dd93dd8a8e12dcdca4c256.

--- PharmaciaOrientadaAObjeto/src/main/java/marketplace/PharmaciaOrientadaAObjeto/controllers/LoginController.java
+++ PharmaciaOrientadaAObjeto/src/main/java/marketplace/PharmaciaOrientadaAObjeto/controllers/LoginController.java
@@ -8,53 +8,52 @@ import marketplace.PharmaciaOrientadaAObjeto.helpers.CadastroService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/auth")
 class LoginController {
-
+    
     private final CadastroService cadastroService;
-    private  final JwtService jwtService;
+    private final JwtService jwtService;
     private final UserService userService;
-    LoginController(CadastroService cadastroService, JwtService jwtService, UserService userService) {
+    
+    LoginController (CadastroService cadastroService, JwtService jwtService, UserService userService) {
         this.cadastroService = cadastroService;
         this.jwtService = jwtService;
         this.userService = userService;
     }
-
+    
     @PostMapping
-    public ResponseEntity<UserDetailsDTO> login(LoginDTO loginDTO, Authentication authentication) {
+    public ResponseEntity<UserDetailsDTO> login (LoginDTO loginDTO, Authentication authentication) {
         try {
-
-
+            
+            
             var result = cadastroService.login(loginDTO);
-
-
-
-            if(result != null) {
-
-               var user = userService.loadUserByUsername(loginDTO.email());
-
-               var token = jwtService.Authenticate(user);
-
-
+            
+            
+            if (result != null) {
+                
+                var user = userService.loadUserByUsername(loginDTO.email());
+                
+                var token = jwtService.Authenticate(user);
+                
+                
                 return ResponseEntity.ok(new UserDetailsDTO(user, token));
             } else {
                 throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Login invalido");
             }
-
-
-        } catch (Exception e) {
-            return  ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            
+            
         }
-
-
-
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        
+        
     }
-
+    
 }
